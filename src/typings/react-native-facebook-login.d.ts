1d6cff33d7ff4a46b478ff255897c6bf043b5e07
Jitae Kim <originerd@gmail.com>
2018-02-20 01:28:08 +0900

    Improve react-native-facebook-login.d.ts
    - add FBLoginResponse interface and FBProfile interface

--- src/typings/react-native-facebook-login.d.ts
+++ src/typings/react-native-facebook-login.d.ts
@@ -18,8 +18,45 @@ declare module 'react-native-facebook-login' {
 
   export class FBLogin extends React.Component<FBLoginProps> {}
 
+  export interface FBProfile {
+    id: string;
+    name: string;
+    email: string;
+    first_name: string;
+    last_name: string;
+    link: string;
+    user_id: string;
+    picture: {
+      data: {
+        height: number;
+        is_silhouette: boolean;
+        url: string;
+        width: number;
+      };
+    };
+    gender: string;
+    locale: string;
+    timezone: number;
+    updated_time: string;
+    verified: boolean;
+  }
+
+  export interface FBLoginResponse {
+    declinedPermissions: Permissions[];
+    provider: 'facebook';
+    eventName: string;
+    profile: FBProfile;
+    type: string;
+    credentials: {
+      permissions: Permissions[];
+      tokenExpirationDate: string;
+      userId: string;
+      token: string;
+    };
+  }
+
   export namespace FBLoginManager {
-    function loginWithPermissions(permissions: Permissions[], callback: (error: Error, data: { credentials: { token: string } }) => void): void;
+    function loginWithPermissions(permissions: Permissions[], callback: (error: Error, data: FBLoginResponse) => void): void;
     function logout(callback: (error: Error, data: { credentials: { token: string } }) => void): void;
   }
 }
