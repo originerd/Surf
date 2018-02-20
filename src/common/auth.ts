acaaba11f7119aeaa533bb0ee6dbb3812c712760
Jitae Kim <originerd@gmail.com>
2018-02-20 21:46:18 +0900

    Modify facebook login function response type
    - from string to FBLoginResponse

--- src/common/auth.ts
+++ src/common/auth.ts
@@ -1,23 +1,23 @@
-import { FBLoginManager } from 'react-native-facebook-login';
+import { FBLoginManager, FBLoginResponse } from 'react-native-facebook-login';
 import { Permissions } from 'react-native-fbsdk';
 
 export namespace Facebook {
-  export const login = (permissions: Permissions[] = ['email']): Promise<string> =>
+  export const login = (permissions: Permissions[] = ['email']): Promise<FBLoginResponse> =>
     new Promise((resolve, reject) => {
       FBLoginManager.loginWithPermissions(permissions, (error, data) => {
         if (!error) {
-          resolve(data.credentials.token);
+          resolve(data);
         } else {
           reject(error);
         }
       });
     });
 
-  export const logout = (): Promise<boolean> =>
+  export const logout = (): Promise<void> =>
     new Promise((resolve, reject) => {
       FBLoginManager.logout((error, data) => {
         if (!error) {
-          resolve(true);
+          resolve();
         } else {
           reject(error);
         }
