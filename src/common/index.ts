3b066f78ee16a014fcc07714968e0053f06044af
Jitae Kim <originerd@gmail.com>
2018-02-26 00:58:54 +0900

    Add Loading component on common

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,10 +1,12 @@
 import * as auth from './auth';
 import EmotionColorsLine from './EmotionColorsLine';
+import Loading from './Loading';
 import { stores, Stores } from './stores';
 
 export {
   auth,
   EmotionColorsLine,
+  Loading,
   stores,
   Stores,
 };
