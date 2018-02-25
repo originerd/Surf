903863405d82a4db58da13ccbe2bb38b4ca9fa8d
Jitae Kim <originerd@gmail.com>
2018-02-26 01:00:46 +0900

    Add session logic and AppNavigation component on App component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,35 @@
 import * as React from 'react';
+import { RNFirebase } from 'react-native-firebase';
+import { observer, Provider } from 'mobx-react';
 
-import Session from '../session';
+import { stores } from '../common';
+import firebase from '../firebase';
+import AppNavigation from '../navigation/AppNavigation';
+import * as Types from '../types';
 
-const App = () => <Session />;
+@observer
+class App extends React.Component {
+  private handleAuthStateChanged = async (user: RNFirebase.User) => {
+    let storedUser: Types.User | undefined;
+
+    if (user) {
+      storedUser = await firebase.database.getUser(user.uid);
+    }
+
+    stores.sessionStore.setUser(storedUser);
+  }
+
+  public componentDidMount() {
+    firebase.auth.onAuthStateChanged(this.handleAuthStateChanged);
+  }
+
+  public render() {
+    return (
+      <Provider {...stores}>
+        <AppNavigation />
+      </Provider>
+    );
+  }
+}
 
 export default App;
