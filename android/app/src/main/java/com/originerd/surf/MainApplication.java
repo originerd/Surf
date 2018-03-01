1a4ad68e417e5d2d91c15c86528040717c932211
Jitae Kim <originerd@gmail.com>
2018-03-01 17:37:13 +0900

    Link react-native-vector-icons

--- android/app/src/main/java/com/originerd/surf/MainApplication.java
+++ android/app/src/main/java/com/originerd/surf/MainApplication.java
@@ -3,6 +3,7 @@ package com.originerd.surf;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNFirebasePackage(),
             new RNFirebaseAuthPackage(),
             new RNFirebaseDatabasePackage(),
