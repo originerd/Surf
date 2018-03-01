ca2300b2ea50f0e1de4e03e5fc83ef00a23f83e5
Jitae Kim <originerd@gmail.com>
2018-03-01 15:27:39 +0900

    Add write button on HomeNavigation

--- src/navigation/HomeNavigation.tsx
+++ src/navigation/HomeNavigation.tsx
@@ -1,13 +1,37 @@
 import * as React from 'react';
-import { Text } from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from 'react-native';
 import { TabNavigator } from 'react-navigation';
 
 import { colors } from '../styles';
 
-const HomeNavigation = TabNavigator(
+const IOS_TAB_HEIGHT = 48.5;
+
+const styles = StyleSheet.create({
+  writeButtonContainer: {
+    backgroundColor: colors.lightBlue,
+    borderRadius: 32,
+    bottom: Platform.select({ android: 0, ios: IOS_TAB_HEIGHT }) + 24,
+    height: 56,
+    position: 'absolute',
+    right: 16,
+    shadowColor: colors.darkerBlue,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: .5,
+    shadowRadius: 2,
+    width: 56,
+  },
+});
+
+const TabNavigation = TabNavigator(
   {
     Timeline: { screen: () => <Text>Timeline</Text> },
-    Surfs: { screen: () => <Text>Surfs</Text> },
+    Ocean: { screen: () => <Text>Ocean</Text> },
     Me: { screen: () => <Text>Me</Text> },
   },
   {
@@ -19,5 +43,18 @@ const HomeNavigation = TabNavigator(
     },
   },
 );
+
+const HomeNavigation = () => (
+  <View style={{ display: 'flex', flex: 1 }}>
+    <TabNavigation />
+    <TouchableHighlight
+      onPress={() => null}
+      style={styles.writeButtonContainer}
+      underlayColor={colors.blue}
+    >
+      <View />
+    </TouchableHighlight>
+  </View>
+)
 
 export default HomeNavigation;
