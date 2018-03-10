08ff7e3000cb8e43eb9913ebb4c2f4bf568b1122
Jitae Kim <originerd@gmail.com>
2018-03-10 23:22:50 +0900

    Add mobx-react types to override inject function

---
+++ src/typings/mobx-react.d.ts
@@ -0,0 +1,14 @@
+import {
+  IReactComponent,
+  IStoresToProps,
+  IWrappedComponent,
+} from 'mobx-react';
+
+declare module 'mobx-react' {
+  export function inject<S, P extends I, I>(
+    fn: IStoresToProps<S, P, I>
+  ): <T extends IReactComponent>(target: T) => IReactComponent<Omit<P, keyof I>> & IWrappedComponent<T>
+  export function inject<S, P extends I, I, C>(
+    fn: IStoresToProps<S, P, I, C>
+  ): <T extends IReactComponent>(target: T) => IReactComponent<Omit<P, keyof I>> & IWrappedComponent<T>
+}
