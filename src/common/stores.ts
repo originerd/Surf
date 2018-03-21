ec88e3bc8507d7b84ae7f0915967e9d8cb59553b
Jitae Kim <originerd@gmail.com>
2018-03-22 01:35:27 +0900

    Add TimelineStore

--- src/common/stores.ts
+++ src/common/stores.ts
@@ -1,19 +1,23 @@
 import SessionStore from '../session/SessonStore';
+import TimelineStore from "../timeline/TimelineStore";
 import UserStore from "../user/UserStore";
 import WriteStore from '../write/WriteStore';
 
 const sessionStore = new SessionStore();
+const timelineStore = new TimelineStore();
 const userStore = new UserStore();
 const writeStore = new WriteStore();
 
 interface Stores {
   sessionStore: SessionStore;
+  timelineStore: TimelineStore;
   userStore: UserStore;
   writeStore: WriteStore;
 }
 
 const stores: Stores = {
   sessionStore,
+  timelineStore,
   userStore,
   writeStore,
 };
