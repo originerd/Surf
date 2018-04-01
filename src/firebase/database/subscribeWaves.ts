93c32a9f3cd4c66646c5639f7e09256126f18ae4
Jitae Kim <originerd@gmail.com>
2018-04-01 22:50:35 +0900

    Add startAt on subscribeWaves function

--- src/firebase/database/subscribeWaves.ts
+++ src/firebase/database/subscribeWaves.ts
@@ -1,7 +1,14 @@
 import firebase, { RNFirebase } from 'react-native-firebase';
 
 import { Types } from '../../common';
+import { getStartAtKey } from './keys';
 
-export const subscribeWaves = async (uid: string, handler: (snapshot: RNFirebase.database.DataSnapshot) => void) => {
-  firebase.database().ref(`waves/${uid}`).orderByChild('createdAt').on('child_added', handler);
+export const subscribeWaves = async (uid: string, startAt: string | undefined, handler: (snapshot: RNFirebase.database.DataSnapshot) => void) => {
+  let wavesRef = firebase.database().ref(`waves/${uid}`).orderByKey();
+
+  if (startAt) {
+    wavesRef = wavesRef.startAt(getStartAtKey(startAt));
+  }
+
+  wavesRef.on('child_added', handler);
 };
