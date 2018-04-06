e3abbbef70b164faa0f3b238f8692f17f2cbee63
Jitae Kim <originerd@gmail.com>
2018-04-07 00:36:24 +0900

    Add Follows on MainNavigation

--- src/navigation/MainNavigation.tsx
+++ src/navigation/MainNavigation.tsx
@@ -1,17 +1,33 @@
 import * as React from 'react';
 import { Text } from 'react-native';
-import { StackNavigator, NavigationScreenProp } from "react-navigation";
+import { StackNavigator, NavigationScreenProp } from 'react-navigation';
 
+import { Types } from '../common';
+import Follows from '../follow/index';
 import { colors } from '../styles';
-import Profile from "../profile";
-import Write from "../write";
+import Profile from '../profile';
+import Write from '../write';
 import HomeNavigation from './HomeNavigation';
+
+interface FollowsNavigationOptionsParams {
+  navigation: NavigationScreenProp<{ params: { type: Types.FollowTypes; uid: string; } }, {}>;
+}
+
+interface ProfileNavigationOptionsParams {
+  navigation: NavigationScreenProp<{ params: { userName: string } }, {}>;
+}
 
 const MainNavigation = StackNavigator(
   {
     Home: { screen: HomeNavigation },
+    Follows: {
+      navigationOptions: ({ navigation }: FollowsNavigationOptionsParams) => ({
+        title: navigation.state.params.type === Types.FollowTypes.followers ? '팔로워' : '팔로잉',
+      }),
+      screen: Follows,
+    },
     Profile: {
-      navigationOptions: ({ navigation }: { navigation: NavigationScreenProp<{ params: { userName: string }},{}> }) => ({
+      navigationOptions: ({ navigation }: ProfileNavigationOptionsParams) => ({
         title: navigation.state.params.userName,
       }),
       screen: Profile,
