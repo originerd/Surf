05bbf0e3217fa71d647a058cb3141fce05bbf210
Jitae Kim <originerd@gmail.com>
2018-04-15 23:26:57 +0900

    Add subscribeCommentCount functions on firebase database

--- src/firebase/database/subscribeCommentCount.ts
+++ src/firebase/database/subscribeCommentCount.ts
@@ -3,12 +3,12 @@ import firebase, { RNFirebase } from 'react-native-firebase';
 import { Types } from '../../common';
 import { getPath, PathTypes } from './getPath';
 
-export const subscribeSympathyCount = (
+export const subscribeCommentCount = (
   waveID: string,
   handler: (snapshot: RNFirebase.database.DataSnapshot) => void,
 ) => {
   firebase
     .database()
-    .ref(getPath({ waveID, path: PathTypes.sympathyCounts  }))
+    .ref(getPath({ waveID, path: PathTypes.commentCounts  }))
     .on('value', handler);
 };
--- src/firebase/database/unsubscribeCommentCount.ts
+++ src/firebase/database/unsubscribeCommentCount.ts
@@ -3,12 +3,9 @@ import firebase, { RNFirebase } from 'react-native-firebase';
 import { Types } from '../../common';
 import { getPath, PathTypes } from './getPath';
 
-export const subscribeSympathyCount = (
+export const unsubscribeCommentCount = (
   waveID: string,
   handler: (snapshot: RNFirebase.database.DataSnapshot) => void,
 ) => {
-  firebase
-    .database()
-    .ref(getPath({ waveID, path: PathTypes.sympathyCounts  }))
-    .on('value', handler);
+  firebase.database().ref(getPath({ waveID, path: PathTypes.commentCounts })).on('value', handler);
 };
