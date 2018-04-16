b769e29746fcda35f0cb255dacbe586cde52fadc
Jitae Kim <originerd@gmail.com>
2018-04-17 01:05:49 +0900

    Add Comments on MainNavigation

--- src/navigation/MainNavigation.tsx
+++ src/navigation/MainNavigation.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Text } from 'react-native';
 import { StackNavigator, NavigationScreenProp } from 'react-navigation';
 
+import Comments from '../comment';
 import { Types } from '../common';
 import Follows from '../follow/index';
 import { colors } from '../styles';
@@ -9,17 +10,27 @@ import Profile from '../profile';
 import Write from '../write';
 import HomeNavigation from './HomeNavigation';
 
-interface FollowsNavigationOptionsParams {
+export interface CommentsNavigationOptionsParams {
+  navigation: NavigationScreenProp<{ params: { wave: Types.Wave } }, {}>;
+}
+
+export interface FollowsNavigationOptionsParams {
   navigation: NavigationScreenProp<{ params: { type: Types.FollowTypes; uid: string; } }, {}>;
 }
 
-interface ProfileNavigationOptionsParams {
-  navigation: NavigationScreenProp<{ params: { userName: string } }, {}>;
+export interface ProfileNavigationOptionsParams {
+  navigation: NavigationScreenProp<{ params: { uid: string; userName: string; } }, {}>;
 }
 
 const MainNavigation = StackNavigator(
   {
     Home: { screen: HomeNavigation },
+    Comments: {
+      navigationOptions: {
+        title: '댓글',
+      },
+      screen: Comments,
+    },
     Follows: {
       navigationOptions: ({ navigation }: FollowsNavigationOptionsParams) => ({
         title: navigation.state.params.type === Types.FollowTypes.followers ? '팔로워' : '팔로잉',
